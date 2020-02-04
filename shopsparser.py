01b5572851d2238b5590035e38804112f0688603
Joost van Viegen <joost@famvanviegen.com>
2020-02-04 19:59:07 +0100

    Parser voor sponsorkliks bedrijven gemaakt

---
+++ shopsparser.py
@@ -0,0 +1,59 @@
+from bs4 import BeautifulSoup
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from selenium import webdriver
+from urllib.parse import urlparse
+import json
+import re
+import time
+
+try:
+    response = get("https://www.sponsorkliks.com/products/shops.php?show=all")
+
+    if(response.status_code == 200):
+        html = BeautifulSoup(response.content, 'html.parser')
+        shopsHtml = html.find_all("a", class_="btn btn-xs btn-primary orderlink")
+        shops = {}
+        i = 0
+        driver = webdriver.Firefox()
+
+        for s in shopsHtml:
+            strings = str(s)
+            print(strings)
+            driver.get("https://sponsorkliks.com" + s.get('href'))
+            time.sleep(10)
+            targetUrl = driver.current_url
+            targetUrl = str(targetUrl)
+            targetUrl = urlparse(targetUrl)
+            targetUrl = targetUrl.hostname
+            targetUrl = targetUrl.replace("www.", "")
+            print(targetUrl)
+
+            domain = targetUrl
+            name = re.findall("shop=([A-z][^&]+)&", strings)
+            id = re.findall("shop_id=([0-9]+)&", strings)
+
+            print ("id = " + id[0])
+            print ("name = " + str(name))
+            print ("domain = " + str(domain))    
+
+            shop = {
+                        "id": id[0],
+                        "name": name[0],
+                        "domain": domain
+            }
+            shops[i] = shop
+            i += 1
+
+        print (json.dumps(shops))
+        file = open("shops.json", "w+")
+        file.write(str(json.dumps(shops)))
+        file.close()
+        driver.quit()
+
+    else:
+        print("er is iets misgegaan")
+
+except RequestException as exception:
+    print("exception" + exception)
